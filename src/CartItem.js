9177595587a37c9292f843bc5a8de28013a52757
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-09-20 19:16:50 +0530

    Modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,43 @@
 import React from 'react'
 
 class CartItem extends React.Component {
+
+    constructor() {
+        super();
+        this.state={
+            title: 'Mobile Phone',
+            price: 39999,
+            qty: 1,
+            img: ''
+        }
+    }
+    increaseQuantity(){
+        this.setState((prev) => {
+            return{
+            qty: prev.qty + 1
+            }
+        })
+    }
+    
     render() {
+        // Object Destructuring
+        const {title, price, qty} = this.state;
+       
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25} }>Phone</div>
-                    <div style={ {color: '#777'} }>Rs. 39999</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
+                    <div style={ {fontSize: 25} }>{title}</div>
+                    <div style={ {color: '#777'} }>Rs. {price}</div>
+                    <div style={ {color: '#777'} }>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                       
+                       <img className="action-icons" alt="increase" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+                       <img className="action-icons" alt="decrease" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" 
+                       onClick={this.increaseQuantity.bind(this)}
+                       />
+                       <img className="action-icons" alt="delete" src="https://cdn-icons-png.flaticon.com/512/709/709519.png" />
                     </div>
                 </div>
             </div>
